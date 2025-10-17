1143f2ba1ada91dcdf0a5d728d3867b03896ee1c
arup <arup@dev.com>
2025-10-17 07:48:29 +0000

    Add test case for mixed newline and comma delimiters.

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -14,4 +14,8 @@ describe "#add" do
   it "returns 10 when the input is '2,4,1,3'" do
     expect(add("2,4,1,3")).to eq(10)
   end
+
+  it 'returns 6 when the input is "1\n2,3"' do
+    expect(add("1\n2,3")).to eq(6)
+  end
 end
