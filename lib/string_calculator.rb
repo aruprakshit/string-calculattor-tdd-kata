26577f1744a74b62c1050f0de246d0dfcf6ab62e
arup <arup@dev.com>
2025-10-17 08:04:14 +0000

    Refactor to OOP design with improved separation of concerns.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,16 +1,66 @@
-def add(input, delimiter = nil)
-  return 0 if input.empty?
+class StringCalculator
+  DEFAULT_DELIMITERS = [',', "\n"].freeze
   
-  delimiters = delimiter ? Array(delimiter) : [',', "\n"]
-  
-  # Validate input for consecutive delimiters
-  delimiter_pattern = Regexp.union(delimiters)
-  if input.match?(/#{delimiter_pattern}#{delimiter_pattern}/)
-    raise RuntimeError, "Invalid string"
+  def self.add(input, delimiter = nil)
+    new(input, delimiter).calculate
   end
   
-  input
-    .split(delimiter_pattern)
-    .map(&:to_i)
-    .sum
+  def initialize(input, delimiter = nil)
+    @input = input
+    @delimiter = delimiter
+  end
+  
+  def calculate
+    return 0 if @input.empty?
+    
+    if custom_delimiter_format?
+      process_custom_delimiter_input
+    else
+      process_standard_input
+    end
+  end
+  
+  private
+  
+  def custom_delimiter_format?
+    @input.start_with?('//')
+  end
+  
+  def process_custom_delimiter_input
+    parts = @input.split("\n", 2)
+    return 0 if parts.length != 2
+    
+    custom_delimiter = parts[0][2..-1]  # Remove '//' prefix
+    numbers_string = parts[1]
+    
+    self.class.add(numbers_string, custom_delimiter)
+  end
+  
+  def process_standard_input
+    validate_input
+    parse_and_sum_numbers
+  end
+  
+  def validate_input
+    delimiters = @delimiter ? Array(@delimiter) : DEFAULT_DELIMITERS
+    delimiter_pattern = Regexp.union(delimiters)
+    
+    if @input.match?(/#{delimiter_pattern}#{delimiter_pattern}/)
+      raise RuntimeError, "Invalid string"
+    end
+  end
+  
+  def parse_and_sum_numbers
+    delimiters = @delimiter ? Array(@delimiter) : DEFAULT_DELIMITERS
+    delimiter_pattern = Regexp.union(delimiters)
+    
+    @input
+      .split(delimiter_pattern)
+      .map(&:to_i)
+      .sum
+  end
+end
+
+def add(input, delimiter = nil)
+  StringCalculator.add(input, delimiter)
 end
