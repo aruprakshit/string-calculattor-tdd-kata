8ba827b5257ce45cefe69ae9332d8e7f4daca2c0
arup <arup@dev.com>
2025-10-17 07:52:05 +0000

    Make delimiter handling more flexible with optional custom delimiter.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,10 @@
-def add(input, delimiter = ',')
+def add(input, delimiter = nil)
   return 0 if input.empty?
   
+  delimiters = delimiter ? Array(delimiter) : [',', "\n"]
+  
   input
-    .split(delimiter)
+    .split(Regexp.union(delimiters))
     .map(&:to_i)
     .sum
 end
