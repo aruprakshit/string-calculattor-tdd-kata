27e956cda2d1b38ff6ea5e16eb634fe68b06255d
arup <arup@dev.com>
2025-10-17 07:32:20 +0000

    Add single number test case and update test wording.

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -1,5 +1,9 @@
 describe "#add" do
-  it "return 0 when the input is an empty string" do
+  it "returns 0 when the input is an empty string" do
     add("")
+  end
+
+  it "returns the number D when the input is 'D'" do
+    expect(add("2")).to eq(2)
   end
 end
