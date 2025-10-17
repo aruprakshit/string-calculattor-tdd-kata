d0ba1acf70dc8a243c1bfd9bee77df1c9c9d3d70
arup <arup@dev.com>
2025-10-17 07:46:09 +0000

    Refactor add method to be more concise and readable.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,12 +1,8 @@
 def add(input, delimiter = ',')
-  if input.empty?
-    0
-  elsif input.length == 1
-    input.to_i
-  else
-    input
-      .split(delimiter)
-      .map(&:to_i)
-      .reduce(0, :+)
-  end
+  return 0 if input.empty?
+  
+  input
+    .split(delimiter)
+    .map(&:to_i)
+    .sum
 end
