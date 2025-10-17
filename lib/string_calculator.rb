db2c9f3b073324492cdcf81d44269e734334c722
arup <arup@dev.com>
2025-10-17 07:39:25 +0000

    Implement comma-separated number addition with configurable delimiter.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,10 @@
-def add(input)
+def add(input, delimiter = ',')
   if input.empty?
-    return 0
-  end
- 
-  if input.length == 1
-    return input.to_i
+    0
+  elsif input.length == 1
+    input.to_i
+  else
+    a, b = input.split(delimiter)
+    a.to_i + b.to_i
   end
 end
